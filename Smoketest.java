7cb690b3e802dda2b95b2cf6a509c98758c80765
unknown <santhoshi.gouru@gmail.com>
2018-07-17 10:29:58 -0700

    commiting testng file

--- Smoketest.java
+++ Smoketest.java
@@ -1,43 +1,58 @@
 package Stepdefs;
 
-import org.openqa.selenium.By;
+import java.io.File;
+import java.lang.reflect.Method;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
 
-import cucumber.api.PendingException;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
+//import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentReporter;
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 
-public class Smoketest {
-	WebDriver driver;
+public class extentreportearn {
+	public static WebDriver driver;
+	public static ExtentReports extent;
+	public static ExtentTest test;
+	@BeforeSuite
+	public void beforesuite(){
+		extent = new ExtentReports("D://reports//myreport.html",true);
+		extent.loadConfig(new File("D//com.learnautomation.cucumber//config files//extent-config.xml"));
+	}
+	@BeforeMethod
+	public void beforemethod(Method method){
+		test= extent.startTest((this.getClass().getName()+"::"+ method.getName()),method.getName());
+		test.assignAuthor("Santhoshi");
+		test.assignCategory("Learning the extent reporting");
+		driver = new ChromeDriver();
+		System.setProperty("webdriver.chrome.driver", "C:/Users/DELL/drivers/new chrome/chromedriver.exe");
+	}
+	@Test
+	public void test() {
+		driver.get("https://www.google.com/");
+		String currenturl = driver.getCurrentUrl();
+		test.log(LogStatus.PASS, "the current page i am on is "+currenturl);
+	}
+	@AfterMethod
+	public void aftermethod() {
+		driver.close();
+		driver.quit();
+		test.log(LogStatus.PASS, "closed the browser");
+		extent.endTest(test);
+	}
+	@AfterSuite
+	public void aftersuite() {
+		extent.flush();
+		extent.close();
+	}
+	
 	
 
-		// TODO Auto-generated method stub
-		@Given("^open the facebook in chrome$")
-		public void open_the_facebook_in_chrome() throws Throwable {
-		    // Write code here that turns the phrase above into concrete actions
-			System.setProperty("webdriver.chrome.driver", "C:/Users/DELL/drivers/new chrome/chromedriver.exe");
-		    driver = new ChromeDriver();
-		    driver.manage().window().maximize();
-		    driver.get("https://www.facebook.com");
-		}
-
-		@Given("^Enter the valid \"([^\"]*)\" and \"([^\"]*)\" and click on log in$")
-		public void enter_the_valid_creds_and_click_on_log_in(String uname, String password ) throws Throwable {
-		    // Write code here that turns the phrase above into concrete actions
-		    driver.findElement(By.id("email")).sendKeys(uname);
-		    driver.findElement(By.id("pass")).sendKeys(password);
-		}
-
-		@Then("^I havee to log in to facebook$")
-		public void i_havee_to_log_in_to_facebook() throws Throwable {
-		    // Write code here that turns the phrase above into concrete actions
-			
-			driver.findElement(By.xpath("//input[@value='Log In']")).click();
-		    
-		}
-
-
-	}
-
-
+}
